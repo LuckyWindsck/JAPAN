16ffee020e1e345cffd44bca865eadcc7d61329d
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-23 03:49:52 +0900

    feat: implement composable use-resas-api

--- src/composables/useResasApi.ts
+++ src/composables/useResasApi.ts
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import type { SearchResponse } from '../types/search-response'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars -- to be implemented
-const fetch = async <T>(path: string): Promise<SearchResponse<T>> => {
-  // To be implemented
+const baseURL = import.meta.env.VITE_RESAS_API_BASE_URL
+const resasAPI = axios.create({ baseURL })
+
+const fetch = async <T>(path: string) => {
+  const response = await resasAPI.get<SearchResponse<T>>(path)
+
+  return response.data
 }
 
 const useResasApi = { fetch }
