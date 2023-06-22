8f52c3ec68aabaac2f60e8ce367f1fa6dfaa2725
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-23 02:21:40 +0900

    feat: implement app-checkbox-prefecture

--- src/components/ThePrefectureSelector.vue
+++ src/components/ThePrefectureSelector.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import AppCheckboxPrefecture from './AppCheckboxPrefecture.vue'
+
+const prefecture = {
+  prefCode: 13,
+  prefName: '東京都',
+}
 </script>
 
 <template>
-  <AppCheckboxPrefecture />
+  <AppCheckboxPrefecture :pref-code="prefecture.prefCode" :pref-name="prefecture.prefName" />
 </template>
 
 <!-- <style scoped></style> -->
