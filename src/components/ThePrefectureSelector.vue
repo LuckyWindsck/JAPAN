41d9a6f7acf71579c60f959ff45ef1c6f2c2d062
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-23 04:45:33 +0900

    feat: implement component the-prefecture-selector

--- src/components/ThePrefectureSelector.vue
+++ src/components/ThePrefectureSelector.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import type { PropType } from 'vue'
 import AppCheckboxPrefecture from './AppCheckboxPrefecture.vue'
+import type { Prefectures } from '../types/search-response'
 
-const prefecture = {
-  prefCode: 13,
-  prefName: '東京都',
-}
+defineProps({
+  prefectures: {
+    type: Object as PropType<Prefectures>,
+    required: true,
+  },
+})
 </script>
 
 <template>
-  <AppCheckboxPrefecture :pref-code="prefecture.prefCode" :pref-name="prefecture.prefName" />
+  <AppCheckboxPrefecture
+    v-for="prefecture of prefectures"
+    :key="prefecture.prefCode"
+    :pref-code="prefecture.prefCode"
+    :pref-name="prefecture.prefName"
+    data-test-class="prefecture-checkbox"
+  />
 </template>
 
 <!-- <style scoped></style> -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
 import TheDataPresenter from '../components/TheDataPresenter.vue'
 import ThePrefectureSelector from '../components/ThePrefectureSelector.vue'
+
+const prefectures = [
+  {
+    prefCode: 11,
+    prefName: '埼玉県',
+  },
+  {
+    prefCode: 12,
+    prefName: '千葉県',
+  },
+  {
+    prefCode: 13,
+    prefName: '東京都',
+  },
+  {
+    prefCode: 14,
+    prefName: '神奈川県',
+  },
+]
 </script>
 
 <template>
   <main>
     <TheDataPresenter />
-    <ThePrefectureSelector />
+    <ThePrefectureSelector :prefectures="prefectures" />
   </main>
 </template>
