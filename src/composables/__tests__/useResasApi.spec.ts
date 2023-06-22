3cdec68de0a6974136b9e256e16c542fab6c137d
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-23 03:47:13 +0900

    test: composable use-resas-api

---
+++ src/composables/__tests__/useResasApi.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from 'vitest'
+import useResasApi from '../useResasApi'
+import type { Prefectures, PopulationComposition } from '../../types/search-response'
+
+const { fetch } = useResasApi
+
+const tokyo = {
+  prefCode: 13,
+  prefName: '東京都',
+}
+
+describe('useResasApi', () => {
+  it('fetch prefectures data from RESAS API', async () => {
+    const data = await fetch<Prefectures>('/api/v1/prefectures')
+    const prefecture = data.result[tokyo.prefCode - 1] // array is 0-indexed
+
+    expect(prefecture.prefCode).toBe(tokyo.prefCode)
+    expect(prefecture.prefName).toBe(tokyo.prefName)
+  })
+
+  it('fetch population composition data from RESAS API', async () => {
+    const data = await fetch<PopulationComposition>(
+      '/api/v1/population/composition/perYear?prefCode=13',
+    )
+    const { boundaryYear } = data.result
+
+    expect(boundaryYear).toEqual(expect.any(Number))
+  })
+})
