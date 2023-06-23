8df2bf5d5fac7397b2fc30ad59b22984e121fb00
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-24 01:25:18 +0900

    feat: implement component the-line-chart

--- src/components/TheDataPresenter.vue
+++ src/components/TheDataPresenter.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import TheLineChart from './TheLineChart.vue'
+
+const [start, end, step] = [1960, 2045, 5]
+const lineCount = 3
+
+const yearCount = (end - start) / 5 + 1
+const years = Array.from({ length: yearCount }, (_, idx) => start + step * idx)
+
+const labels = years.map(String)
+const datasets = Array.from({ length: lineCount }, (_, i) => i + 1).map((idx) => ({
+  label: `東京都-${idx}`,
+  data: Array.from({ length: yearCount }, () => 10 ** 7 * (0.75 + Math.random() / 2)),
+}))
 </script>
 
 <template>
-  <TheLineChart />
+  <TheLineChart :labels="labels" :datasets="datasets" />
 </template>
 
 <!-- <style scoped></style> -->
--- src/components/__tests__/TheLineChart.spec.ts
+++ src/components/__tests__/TheLineChart.spec.ts
@@ -3,15 +3,30 @@ import { describe, expect, it } from 'vitest'
 
 import TheLineChart from '../TheLineChart.vue'
 
-// Since we shouldn't check its implementation, we can only check if there is an element which is in
-// charge of rendering data in a line chart. To test whether it renders data correctly, we should
-// use a visual regression test, but that is not the responsibility for this component test.
+// Source: https://github.com/hustcc/jest-canvas-mock/issues/2#issuecomment-369608229
+//
+// npm package `canvas` has been added as a devDependency to resolve the
+// following error:
+//
+// > stderr | src/components/__tests__/TheLineChart.spec.ts > TheLineChart > renders a line chart
+// > Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
+//
+// TODO: check which package is better to use, `canvas` or `jest-canvas-mock`.
+
+// Q1: Why there is no test for how line chart render data?
+// A1: Since we shouldn't check its implementation, we can only check if there is an element which
+//     is in charge of rendering data in a line chart. To test whether it renders data correctly, we
+//     should use a visual regression test, but that is not the responsibility for this component
+//     test.
 
 describe('TheLineChart', () => {
   it('renders a line chart', () => {
-    const data = {}
+    const props = {
+      labels: [],
+      datasets: [],
+    }
 
-    const wrapper = mount(TheLineChart, { props: { data } })
+    const wrapper = mount(TheLineChart, { props })
     const lintChart = wrapper.findAll('[data-test-class="line-chart"]')
 
     expect(lintChart.length).toBe(1)
