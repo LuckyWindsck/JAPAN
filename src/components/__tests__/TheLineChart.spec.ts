9411e26fd9b35cf08d141e44b1f77b5807322c87
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-24 15:36:22 +0900

    fix: implementation error during component test

--- src/components/__tests__/TheLineChart.spec.ts
+++ src/components/__tests__/TheLineChart.spec.ts
@@ -3,16 +3,6 @@ import { describe, expect, it } from 'vitest'
 
 import TheLineChart from '../TheLineChart.vue'
 
-// Source: https://github.com/hustcc/jest-canvas-mock/issues/2#issuecomment-369608229
-//
-// npm package `canvas` has been added as a devDependency to resolve the
-// following error:
-//
-// > stderr | src/components/__tests__/TheLineChart.spec.ts > TheLineChart > renders a line chart
-// > Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
-//
-// TODO: check which package is better to use, `canvas` or `jest-canvas-mock`.
-
 // Q1: Why there is no test for how line chart render data?
 // A1: Since we shouldn't check its implementation, we can only check if there is an element which
 //     is in charge of rendering data in a line chart. To test whether it renders data correctly, we
@@ -21,14 +11,15 @@ import TheLineChart from '../TheLineChart.vue'
 
 describe('TheLineChart', () => {
   it('renders a line chart', () => {
-    const props = {
-      labels: [],
-      datasets: [],
-    }
-
-    const wrapper = mount(TheLineChart, { props })
+    const wrapper = mount(TheLineChart, {
+      shallow: true,
+      props: {
+        labels: [],
+        datasets: [],
+      },
+    })
     const lintChart = wrapper.findAll('[data-test-class="line-chart"]')
 
-    expect(lintChart.length).toBe(1)
+    expect(lintChart).toHaveLength(1)
   })
 })
