7aebe6b29180792a0b426278f423184370404d31
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-25 20:46:00 +0900

    test: filter prefectures based on whether they are selected or not

--- src/stores/prefectures.spec.ts
+++ src/stores/prefectures.spec.ts
@@ -1,7 +1,21 @@
+import { fakerJA } from '@faker-js/faker'
 import { createPinia, setActivePinia } from 'pinia'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import usePrefecturesStore from '@/stores/prefectures'
+
+import type { Prefecture } from '@/types/prefecture'
+
+const prefectureCountInJapan = 47
+const randomSelectedPrefectureCount = fakerJA.number.int({ min: 1, max: prefectureCountInJapan })
+const nonSelectedPrefectureCount = prefectureCountInJapan - randomSelectedPrefectureCount
+
+const generateRandomPrefectureWithOption = (isSelected: boolean): Prefecture => ({
+  prefCode: fakerJA.number.int({ min: 1, max: prefectureCountInJapan }),
+  prefName: fakerJA.location.state(),
+  populationComposition: null,
+  isSelected,
+})
 
 describe('Prefectures Store', () => {
   beforeEach(() => {
@@ -12,5 +26,24 @@ describe('Prefectures Store', () => {
     const prefecturesStore = usePrefecturesStore()
 
     expect(prefecturesStore.prefectures).toHaveLength(0)
+  })
+
+  it('filters selected and non-selected prefectures', () => {
+    const prefecturesStore = usePrefecturesStore()
+
+    const selectedPrefectures = Array.from({ length: randomSelectedPrefectureCount }, () =>
+      generateRandomPrefectureWithOption(true),
+    )
+    const nonSelectedPrefectures = Array.from({ length: nonSelectedPrefectureCount }, () =>
+      generateRandomPrefectureWithOption(false),
+    )
+    prefecturesStore.prefectures = fakerJA.helpers.shuffle([
+      ...selectedPrefectures,
+      ...nonSelectedPrefectures,
+    ])
+
+    expect(prefecturesStore.prefectures).toHaveLength(prefectureCountInJapan)
+    expect(prefecturesStore.selectedPrefectures).toHaveLength(randomSelectedPrefectureCount)
+    expect(prefecturesStore.nonSelectedPrefectures).toHaveLength(nonSelectedPrefectureCount)
   })
 })
