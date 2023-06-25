04a0d120ab8aca0ada9d783e515981429efa4283
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-25 20:40:02 +0900

    feat: fetch population composition

--- src/components/ThePrefectureSelector.vue
+++ src/components/ThePrefectureSelector.vue
@@ -1,29 +1,47 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import { watch } from 'vue'
 
 import AppCheckboxPrefecture from '@/components/AppCheckboxPrefecture.vue'
+import useResasApi from '@/composables/useResasApi'
 import usePrefecturesStore from '@/stores/prefectures'
 
 import type { Prefecture } from '@/types/prefecture'
+import type { PopulationCompositionData } from '@/types/search-response'
 
 const prefecturesStore = usePrefecturesStore()
 const { prefectures } = storeToRefs(prefecturesStore)
 
-// We can extract type of a single emit by InstanceType<TComponent>['$emit']. However for a
-// component with multiple emits defined, the type we extercted will be an intersection type of emit
-// function types. It seems to be difficult to extract a specific emit function type, so what we can
-// do is to ensure the type here match the parameter types of respective emit.
-//
-// Also, we are not sure why the following code doesn't work:
-// > const fn = (prefecture) => (newValue) => { prefecture.isSelected = newValue }
-// > @update:is-selected="fn(prefecture)"
+/* eslint-disable no-param-reassign */
+const updatePrefecture = (prefecture: Prefecture) => {
+  // We can extract type of a single emit by InstanceType<TComponent>['$emit']. However for a
+  // component with multiple emits defined, the type we extercted will be an intersection type of emit
+  // function types. It seems to be difficult to extract a specific emit function type, so what we can
+  // do is to ensure the type here match the parameter types of respective emit.
+  return (newValue: boolean) => {
+    prefecture.isSelected = newValue
 
-const updatePrefecture = (prefecture: Prefecture) => (newValue: boolean) =>
-  Object.assign(prefecture, { isSelected: newValue })
+    if (prefecture.isSelected && prefecture.populationComposition === null) {
+      const path = `/api/v1/population/composition/perYear?prefCode=${prefecture.prefCode}`
+      const { data, error, isLoading } = useResasApi<PopulationCompositionData>(path)
+
+      watch(isLoading, () => {
+        if (error.value instanceof Error) throw error.value
+        if (data.value === undefined) return
+
+        prefecture.populationComposition = data.value.result
+      })
+    }
+  }
+}
+/* eslint-enable no-param-reassign */
 </script>
 
 <template>
   <div class="prefecture-selector">
+    <!-- We are not sure why the following code doesn't work:
+         > @update:is-selected="updatePrefecture(prefecture)"
+         So we wrap updatePrefecture to solve this problem. -->
     <AppCheckboxPrefecture
       v-for="prefecture of prefectures"
       :key="prefecture.prefCode"
--- src/composables/useResasApi.ts
+++ src/composables/useResasApi.ts
@@ -6,6 +6,6 @@ import type { SearchResponse } from '@/types/search-response'
 const baseURL = import.meta.env.VITE_RESAS_API_BASE_URL
 const resasAPI = axios.create({ baseURL })
 
-const useResasApi = <T>(url: string) => useAxios<SearchResponse<T>>(url, resasAPI)
+const useResasApi = <T>(path: string) => useAxios<SearchResponse<T>>(path, resasAPI)
 
 export default useResasApi
