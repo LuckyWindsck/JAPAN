cd4d194dcbd5f0e9bbf9102138d8242c6081a204
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-25 20:53:43 +0900

    feat: prefectures filtering

--- src/stores/prefectures.ts
+++ src/stores/prefectures.ts
@@ -1,12 +1,22 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 import type { Prefecture } from '@/types/prefecture'
 
 const usePrefecturesStore = defineStore('prefectures', () => {
   const prefectures = ref<Prefecture[]>([])
+  const selectedPrefectures = computed(() =>
+    prefectures.value.filter((prefecture) => prefecture.isSelected),
+  )
+  const nonSelectedPrefectures = computed(() =>
+    prefectures.value.filter((prefecture) => !prefecture.isSelected),
+  )
 
-  return { prefectures }
+  return {
+    prefectures,
+    selectedPrefectures,
+    nonSelectedPrefectures,
+  }
 })
 
 export default usePrefecturesStore
