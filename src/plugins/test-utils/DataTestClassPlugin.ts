2d09db3c37997840b946fd564848b81f7af46385
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-26 23:22:19 +0900

    feat: implement plugin method find-by-test-class

--- src/plugins/test-utils/DataTestClassPlugin.ts
+++ src/plugins/test-utils/DataTestClassPlugin.ts
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { VueWrapper } from '@vue/test-utils'
 
 const DataTestClassPlugin = (wrapper: VueWrapper) => {
-  const findByTestClass: typeof wrapper.findByTestClass = (selector) => {
-    // To be implemented
-  }
+  const findByTestClass: typeof wrapper.findByTestClass = (selector) =>
+    wrapper.find(`[data-test-class="${selector}"]`)
 
   const findAllByTestClass: typeof wrapper.findAllByTestClass = (selector) =>
     wrapper.findAll(`[data-test-class="${selector}"]`)
