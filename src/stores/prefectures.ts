f2577f9d8d14c438438b16f4dc9da6d6a987c0b1
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-26 09:39:39 +0900

    test: select specified prefecture

--- src/stores/prefectures.ts
+++ src/stores/prefectures.ts
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 import type { Prefecture } from '@/types/prefecture'
 
 const usePrefecturesStore = defineStore('prefectures', () => {
+  // States
+  const defaultPrefecture = ref(/* To be implemented */)
   const prefectures = ref<Prefecture[]>([])
+
+  // Getters
   const selectedPrefectures = computed(() =>
     prefectures.value.filter((prefecture) => prefecture.isSelected),
   )
@@ -12,10 +17,24 @@ const usePrefecturesStore = defineStore('prefectures', () => {
     prefectures.value.filter((prefecture) => !prefecture.isSelected),
   )
 
+  // Actions
+  const selectPrefecture = (selectPrefName) => {
+    /* To be implemented */
+  }
+
+  const selectDefaultPrefecture = () => {
+    /* To be implemented */
+  }
+
   return {
+    defaultPrefecture,
     prefectures,
+
     selectedPrefectures,
     nonSelectedPrefectures,
+
+    selectPrefecture,
+    selectDefaultPrefecture,
   }
 })
 
