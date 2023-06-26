47b6d9eaed8ef03c19578448c93a63cd2d287fa3
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-26 23:17:10 +0900

    test: plugin method find-by-test-class

--- src/vue-test-utils.d.ts
+++ src/vue-test-utils.d.ts
@@ -5,6 +5,7 @@ declare module '@vue/test-utils' {
   export class VueWrapper {
     // We MUST ensure that @/plugins/test-utils/DataTestClassPlugin is installed on VueWrapper
     // We install the plugin in @/configs/vitest/setup.ts
+    findByTestClass(selector: string): DOMWrapper<Element>
     findAllByTestClass(selector: string): DOMWrapper<Element>[]
   }
 }
