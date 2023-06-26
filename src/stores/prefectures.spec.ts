e12ffe60c0df0bef0ddfe9349080f3112a65952b
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-26 09:39:39 +0900

    test: select specified prefecture

--- src/stores/prefectures.spec.ts
+++ src/stores/prefectures.spec.ts
@@ -4,14 +4,19 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import usePrefecturesStore from '@/stores/prefectures'
 import { integerFactory, prefectureCountInJapan } from '@/utils/test/factories/faker'
 import prefecturesFactory from '@/utils/test/factories/prefectures'
+import { tokyo } from '@/utils/test/fixtures/prefectures'
 import { shuffle } from '@/utils/test/helpers/faker'
-
-const selectedPrefectureCount = integerFactory({ max: prefectureCountInJapan })
-const nonSelectedPrefectureCount = prefectureCountInJapan - selectedPrefectureCount
 
 describe('Prefectures Store', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
+  })
+
+  // States
+  it('specify Tokyo as default prefecture to select', () => {
+    const prefecturesStore = usePrefecturesStore()
+
+    expect(prefecturesStore.defaultPrefecture).toContain('東京')
   })
 
   it('contains no prefecture initially', () => {
@@ -20,9 +25,12 @@ describe('Prefectures Store', () => {
     expect(prefecturesStore.prefectures).toHaveLength(0)
   })
 
+  // Getters
   it('filters selected and non-selected prefectures', () => {
     const prefecturesStore = usePrefecturesStore()
 
+    const selectedPrefectureCount = integerFactory({ max: prefectureCountInJapan })
+    const nonSelectedPrefectureCount = prefectureCountInJapan - selectedPrefectureCount
     const selectedPrefectures = prefecturesFactory(selectedPrefectureCount, { isSelected: true })
     const nonSelectedPrefectures = prefecturesFactory(nonSelectedPrefectureCount, {
       isSelected: false,
@@ -32,5 +40,28 @@ describe('Prefectures Store', () => {
     expect(prefecturesStore.prefectures).toHaveLength(prefectureCountInJapan)
     expect(prefecturesStore.selectedPrefectures).toHaveLength(selectedPrefectureCount)
     expect(prefecturesStore.nonSelectedPrefectures).toHaveLength(nonSelectedPrefectureCount)
+  })
+
+  // Actions
+  it('select specified prefecture', () => {
+    const prefectureCount = integerFactory({ min: 1, max: prefectureCountInJapan })
+    const randomIndex = integerFactory({ min: 0, max: prefectureCount - 1 })
+    const prefectures = prefecturesFactory(prefectureCount)
+    const prefecturesStore = usePrefecturesStore()
+    prefecturesStore.prefectures = prefectures
+    const prefecture = prefectures[randomIndex]
+
+    prefecturesStore.selectPrefecture(prefecture.prefName)
+
+    expect(prefecture.isSelected).toBeTruthy()
+  })
+
+  it('select default prefecture', () => {
+    const prefecturesStore = usePrefecturesStore()
+    prefecturesStore.prefectures = [tokyo]
+
+    prefecturesStore.selectDefaultPrefecture()
+
+    expect(tokyo.isSelected).toBeTruthy()
   })
 })
