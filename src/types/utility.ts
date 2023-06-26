03ceeffe7c1bbfb89aa4d3a2758cb0170499d2f9
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-26 11:47:30 +0900

    refactor: move type definition

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -1,4 +1,12 @@
+import type { ChartTypeRegistry } from 'chart.js'
+import type { TypedChartComponent } from 'node_modules/vue-chartjs/dist/types'
 import type { AllowedComponentProps, Component, VNodeProps } from 'vue'
+
+export type ChartType<ChartComponent> = ChartComponent extends TypedChartComponent<
+  infer TType extends keyof ChartTypeRegistry
+>
+  ? TType
+  : never
 
 // Source: https://stackoverflow.com/questions/68602712/extracting-the-prop-types-of-a-component-in-vue-3-typescript-to-use-them-somew#answer-73784241
 export type ComponentProps<C extends Component> = C extends new (...args: unknown[]) => unknown
