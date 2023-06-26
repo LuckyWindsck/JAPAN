1b933520536c8de6d155b3ff66c5b74c165fb161
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-27 07:47:46 +0900

    fix: avoid test failure by generating unique prefectures with factory

--- src/utils/test/factories/prefectures.ts
+++ src/utils/test/factories/prefectures.ts
@@ -1,10 +1,32 @@
-import prefactureFactory from '@/utils/test/factories/prefecture'
+import { fakerJA } from '@faker-js/faker'
 
+import { shuffle } from '../helpers/faker'
+
+import type { Prefecture } from '@/types/prefecture'
 import type { PrefactureFactoryOption } from '@/utils/test/factories/prefecture'
 
-type PrefacturesFactoryOption = PrefactureFactoryOption
+type PrefacturesFactoryOption = Pick<
+  PrefactureFactoryOption,
+  'populationComposition' | 'isSelected'
+>
 
-const prefecturesFactory = (length: number, options: PrefacturesFactoryOption = {}) =>
-  Array.from({ length }, () => prefactureFactory(options))
+// We MUST generate UNIQUE prefectures array to avoid test failures.
+// In practice, we assumed that RESAS API doesn't contain duplicate prefectures. Therefore, our
+// implementation might fail some tests if we generate random prefectures with duplicate value.
+const prefecturesFactory = (length: number, options: PrefacturesFactoryOption = {}) => {
+  // Workaround: .slice() to convert readonly array to array
+  const prefectureNames: Prefecture['prefName'][] = fakerJA.definitions.location.state.slice()
+
+  const prefectures: Prefecture[] = prefectureNames.map((prefName, index) => ({
+    prefCode: index - 1,
+    prefName,
+    populationComposition: null,
+    isSelected: false,
+    ...options,
+  }))
+
+  // No need to limit `length`, as Array.prototype.slice handles it.
+  return shuffle(prefectures).slice(0, length)
+}
 
 export default prefecturesFactory
