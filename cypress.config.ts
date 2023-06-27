9c5a192019d4a05476cbf80c636c6da339fbaffa
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-27 12:25:42 +0900

    build: load environment variables in cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'cypress'
+import { loadEnv } from 'vite'
+
+// Allow `loadEnv` to use default `prefixes`, since we didn't set `vite.config.envPrefix`.
+const mode = process.env.NODE_ENV ?? ''
+const envDir = process.cwd()
+const env = loadEnv(mode, envDir)
 
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173/japan/',
   },
+  env,
 })
