9f76398974d54188bd4cac3fec47e945b255911a
LuckyWind_sck <luckywindsck@gmail.com>
2023-06-27 20:58:46 +0900

    feat(component): implement the-type-switcher-population

--- src/components/TheTypeSwitcherPopulation.spec.ts
+++ src/components/TheTypeSwitcherPopulation.spec.ts
@@ -2,13 +2,21 @@ import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 
 import TheTypeSwitcherPopulation from '@/components/TheTypeSwitcherPopulation.vue'
+import { useConstants } from '@/composables/useConstants'
+
+const { defaultSelectedDataType, populationCompositionDataLabels } = useConstants()
 
 describe.concurrent('TheTypeSwitcherPopulation', () => {
-  it('renders 4 radio buttons for labels', () => {
-    const wrapper = mount(TheTypeSwitcherPopulation)
+  it('renders radio buttons for labels', () => {
+    const wrapper = mount(TheTypeSwitcherPopulation, {
+      global: {
+        provide: { selectedDataType: defaultSelectedDataType },
+      },
+    })
+    const labelCount = populationCompositionDataLabels.length
 
     const radioButtons = wrapper.findAllByTestClass('data-type-radio-button')
 
-    expect(radioButtons).toHaveLength(4)
+    expect(radioButtons).toHaveLength(labelCount)
   })
 })
